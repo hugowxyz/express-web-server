75aa177ae698bbb9eb116db4ffb37dad818871de
Hugo <phanhuyhugo@gmail.com>
2020-03-30 09:05:46 +0100

    adding front-end production build

---
+++ build/precache-manifest.2db64942e671d913c555e5f0efd33edf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18d1172696c7a4b116430a69982f500",
+    "url": "/index.html"
+  },
+  {
+    "revision": "016f6b0c3cde91547d23",
+    "url": "/static/css/main.6a1d026a.chunk.css"
+  },
+  {
+    "revision": "ef02f41fe8fac4399ac0",
+    "url": "/static/js/2.29bbd19a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.29bbd19a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "016f6b0c3cde91547d23",
+    "url": "/static/js/main.eb59d926.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
---
+++ build/precache-manifest.c314bf4597b52eb214d843a2bcbcd18c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e104bf39729c0b1071f3e437ca79722",
+    "url": "/index.html"
+  },
+  {
+    "revision": "306f88345b0d30dd76b4",
+    "url": "/static/css/main.6a1d026a.chunk.css"
+  },
+  {
+    "revision": "ef02f41fe8fac4399ac0",
+    "url": "/static/js/2.29bbd19a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.29bbd19a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "306f88345b0d30dd76b4",
+    "url": "/static/js/main.5dcc0ad8.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
